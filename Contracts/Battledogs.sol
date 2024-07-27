ff59bf558218747635a368ae57c56e3e05c30f0c
0xSorcerers <121747420+0xsorcerers@users.noreply.github.com>
2024-07-27 08:44:05 +0100

    Update Battledogs.sol

--- Contracts/Battledogs.sol
+++ Contracts/Battledogs.sol
@@ -3,6 +3,7 @@
 // @title NFT Game by OxSorcerers for Battledog Games (Linea)
 // https://twitter.com/0xSorcerers | https://github.com/Dark-Viper | https://t.me/Oxsorcerer | https://t.me/battousainakamoto | https://t.me/darcViper
 
+
 pragma solidity ^0.8.17;
 
 interface Iburn {
@@ -538,24 +539,11 @@ contract battledog is ERC721Enumerable, Ownable, ReentrancyGuard {
         uint256 counter = 0;        
         for (uint256 i = 0; i < total; i++) {
           uint256 tokenId = tokenOfOwnerByIndex(_player, i);
-                result[tokenId] = players[tokenId];
+                result[counter] = players[tokenId];
                 counter++;
         }
         return result;
     } 
-
-    // function getPlayerOwners(address _player) public view returns (Player[] memory) {
-    //     Player[] memory result = new Player[](balanceOf(_player));
-    //     uint256 counter = 0;        
-    //     uint256 total = totalSupply();
-    //     for (uint256 i = 0; i < total; i++) {
-    //         if (ownerOf(i) == _player) {
-    //             result[counter] = players[i];
-    //             counter++;
-    //         }
-    //     }
-    //     return result;
-    // } 
     
     function addAssaulter(uint256 attackerId, uint256 defenderId, uint256 stolenPoints) internal {
         Assaulter memory assaulter = Assaulter({
